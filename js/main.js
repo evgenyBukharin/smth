3cba4df83bc3410317d3e2274512274a8c608d31
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-09-21 14:38:05 +0500

    done?

--- js/main.js
+++ js/main.js
@@ -21,11 +21,19 @@ $(function () {
         slidesPerView: 1,
         loop: false,
         spaceBetween: 20,
-        direction: 'vertical',
+
         pagination: {
             el: '.advantages__dots',
             type: 'bullets',
             clickable: true,
+        },
+        breakpoints: {
+            0: {
+                direction: 'horizontal',
+            },
+            769: {
+                direction: 'vertical',
+            },
         },
         //  mousewheel: {
         //     releaseOnEdges: 'true'
@@ -51,6 +59,8 @@ $(function () {
         // },
     });
 
+    const media576 = window.matchMedia('(max-width: 576px)');
+
     const advantagesOverlay = new Swiper('.advantages__overlay', {
         slidesPerView: 1,
         loop: false,
@@ -66,10 +76,13 @@ $(function () {
         on: {
             slideChange: function () {
                 let height = $('.advantages__img img').height() - $('.advantages__img').height();
+                console.log($('.advantages__img img').height(), $('.advantages__img').height());
                 let part = height / ($('.advantage').length - 1);
                 let shift = part * advantagesSlider.activeIndex;
                 if (height > 0) {
-                    $('.advantages__img img').css('top', -shift + 'px');
+                    $('.advantages__img img').css('transform', `translateY(${-shift + 'px'})`);
+                } else {
+                    $('.advantages__img img').css('transform', `translateY(${shift + 'px'})`);
                 }
             },
         },
